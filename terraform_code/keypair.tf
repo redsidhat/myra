43510084163f923bfa50d09341176c1cad9c1212
Appu Sidharth <sidsidhardh@gmail.com>
2017-07-28 11:00:06 +0530

    ami keypair and variables

---
+++ terraform_code/keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "wp-key" {
+  key_name   = "wp-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuI4Ph0rxNOoGx6XJhzH8bv3dCqGq2czBtgJY03XaRRGVsiJfYRayn7SFJrgNJ1sxxXOMaNxDwHmmC0/W4bFB6v/avzGWkTRDXAtWztQNlS/TiOfT6zAhqiPaFmOWA0PYmSHuw+nYFi4maeTj6g0k7I9cLMS391Ig/27gWUJL5ay7S16Fgi78bByb0ZXqqTL+VZdh7tO+401iufo0QT1xZ35f6pOqLCjfInlu5saDC+l5d+Iqk4N/fBkIyoFX/t1IO/F2x+eFH/o20yQYzD/L+tLAngGrunl+jr7Q8aDRo1xqa+oer3oF7eaZd3axYka4wKbnfce2TPiIidYjD3E+b appu@acer"
+}
