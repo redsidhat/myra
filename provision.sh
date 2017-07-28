334b503a26d9a43733f964dfe4f781d91f3018a1
Appu Sidharth <sidsidhardh@gmail.com>
2017-07-28 11:10:35 +0530

    launching server

--- provision.sh
+++ provision.sh
@@ -30,13 +30,25 @@ if [ ! -f terraform ]; then
     echo "cleaning up"
     rm -rf terraform.zip
 else
-	echo "Terraform is present"
+	echo "Terraform is present; proceeding"
 fi
 echo "Checking if the keypair exist in the directory"
-if [ ! -f keyfile || ! -f keyfile.pub ]; then
+if [ ! -f keyfile ] || [ ! -f keyfile.pub ]; then
 	echo "Keypair not present. Generating new keypair"
 	ssh-keygen -b 2048 -t rsa -f ./keyfile -q -N ""
     if [ $? -ne 0 ]; then
     	print "keyfile generation failed"
     	exit 1
     fi
+else
+	echo "Keypair found; proceeding"
+fi
+PUB_KEY=`cat keyfile.pub`
+echo "Updating terraform keypair class with new public key"
+REPLACE="  public_key = \\\"$PUB_KEY\\\""
+#Following line does a simple sed replace in keypair.tf. 
+#Did not use direct file replacement to ensure compatibility on linux and BSD based OSs
+cd terraform_code
+echo `cat keypair.tf |sed -e "s|.*public_key.*|$REPLACE|g" > keypair.tf.bk && mv keypair.tf.bk keypair.tf`
+echo "Running terraform plan"
+../terraform plan
